962973ffcd0275be7c986918851f303ba043e7aa
takeitfromtheman <v.khristich@gmail.com>
2025-03-25 20:21:28 +0300

    Write the logic to get the computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello, World!');
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+function getComputerChoice(){
+    const randomInt3 = getRandomInt(3);
+    switch(randomInt3){
+        case 0: return "Rock"
+        case 1: return "Paper"
+        case 2: return "Scissors"
+    }
+}
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
